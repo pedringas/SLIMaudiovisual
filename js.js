e311452964d842a6b496abc4fbb1f33e7590bfb1
Pconti10 <pconti10@gmail.com>
2023-01-24 18:25:34 -0300

    casi terminado

--- js.js
+++ js.js
@@ -131,13 +131,4 @@ $(function () {
 });
 
 
-const DESKTOP_BREAKPOINT = 480;
-window.addEventListener("load",function() {
-  let heroVideoEl = document.getElementByID("hero-video");
-  let heroVideoSrc = heroVideoEl.dataset.srcMobile
-  let viewportWidth = window.innerWidth;
-  if (viewportWidth >= DESKTOP_BREAKPOINT) {
-    heroVideoSrc = heroVideoEl.dataset.srcDesktop;
-  }
-  heroVideoEl.src = heroVideoSrc;
-})
+objectFitVideos();
